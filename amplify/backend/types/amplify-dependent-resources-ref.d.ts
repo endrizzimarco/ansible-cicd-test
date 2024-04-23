1a5767b2c8bf5d788cae71b3a0e852e42a0c5167
Marco Endrizzi <m.endrizzi@reply.com>
2024-04-23 21:22:23 +0100

    brah

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++
@@ -1,9 +0,0 @@
-export type AmplifyDependentResourcesAttributes = {
-  "api": {
-    "letsgo": {
-      "GraphQLAPIEndpointOutput": "string",
-      "GraphQLAPIIdOutput": "string",
-      "GraphQLAPIKeyOutput": "string"
-    }
-  }
-}
